29f6546174f0bf72c0e80dc403aa42b9db6b4f88
jkvim <jkvim@outlook.com>
2017-01-11 03:50:25 +0800

    Refactor test case of LoopRunnerTest

--- src/test/java/LoopRunnerTest.java
+++ src/test/java/LoopRunnerTest.java
@@ -7,7 +7,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class LoopRunnerTest {
     @Test
-    public void test_run_should_with_range_time() {
-        assertThat(LoopRunner.run(1, 4)).isEqualTo("12Fizz");
+    public void test_run_should_return_12_between_1_and_3() {
+        assertThat(LoopRunner.run(1, 3)).isEqualTo("12");
     }
 }
