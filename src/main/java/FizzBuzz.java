be4a3d7e973909b71998e4852e08ced61635b156
jkvim <jkvim@outlook.com>
2017-01-11 02:21:07 +0800

    add FizzBuzz implement and unit test

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zhlin on 1/11/17.
+ */
+class FizzBuzz {
+    static String getCode(int number) {
+        return number + "";
+    }
+}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by zhlin on 1/11/17.
  */
 public class FizzBuzzTest {
+    @Test
+    public void test_getcode_should_return_1_given_1() {
+        assertThat(FizzBuzz.getCode(1)).isEqualTo("1");
+    }
 }
