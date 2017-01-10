f944ef2b945a4828162769366ed43d1343183ab0
jkvim <jkvim@outlook.com>
2017-01-11 02:22:29 +0800

    add Test annotation to FizzBuzzCheckerTest

--- src/test/java/FizzBuzzCheckerTest.java
+++ src/test/java/FizzBuzzCheckerTest.java
@@ -3,21 +3,25 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FizzBuzzCheckerTest {
+    @Test
     public void test_isFizz_should_return_false() {
         assertThat(FizzBuzzChecker.isFizz(1)).isFalse();
         assertThat(FizzBuzzChecker.isFizz(5)).isFalse();
     }
 
+    @Test
     public void test_isFizz_should_return_true() {
         assertThat(FizzBuzzChecker.isFizz(3)).isTrue();
         assertThat(FizzBuzzChecker.isFizz(9)).isTrue();
     }
 
+    @Test
     public void test_isBuzz_should_return_false() {
         assertThat(FizzBuzzChecker.isBuzz(2)).isFalse();
         assertThat(FizzBuzzChecker.isBuzz(3)).isFalse();
     }
 
+    @Test
     public void test_isBuzz_should_return_true() {
         assertThat(FizzBuzzChecker.isBuzz(5)).isTrue();
         assertThat(FizzBuzzChecker.isBuzz(10)).isTrue();
