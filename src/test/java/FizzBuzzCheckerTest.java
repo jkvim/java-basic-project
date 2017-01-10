28d82b21de5f56d99ad11195122cf0871d02ab93
jkvim <jkvim@outlook.com>
2017-01-11 01:56:25 +0800

    add unit test case of isFizz

--- src/test/java/FizzBuzzCheckerTest.java
+++ src/test/java/FizzBuzzCheckerTest.java
@@ -7,4 +7,9 @@ public class FizzBuzzCheckerTest {
         assertThat(FizzBuzzChecker.isFizz(1)).isFalse();
         assertThat(FizzBuzzChecker.isFizz(5)).isFalse();
     }
+
+    public void test_isFizz_should_return_true() {
+        assertThat(FizzBuzzChecker.isFizz(3)).isTrue();
+        assertThat(FizzBuzzChecker.isFizz(9)).isTrue();
+    }
 }
